62360795c878720f88e2fc60598e49298dfa2783
InfinityMo <68720016+InfinityMo@users.noreply.github.com>
2020-12-07 18:18:25 +0800

    2020-12-07

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div id="app"
+         v-loading="$store.state.spinning"
+         element-loading-text="加载中，请稍后..."
+         element-loading-background="rgba(0, 0, 0, .6)">
+      <router-view />
+
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="less">
+</style>
---
+++ src/views/link/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="page">
+    <div class="title">
+      套装分类小工具
+    </div>
+    <div class="content-wrap">
+      <ul>
+        <li>
+          <h6>悦诗风吟护肤洁面套装新春限定礼盒装，拍一发10，送小样悦诗风吟护肤洁面套装新春限定礼盒装，拍一发10，送小样</h6>
+          <div class="swiper-wrap">
+            <swiper :options="swiperOption">
+              <swiper-slide class="item">0</swiper-slide>
+              <swiper-slide>Slide 1</swiper-slide>
+              <swiper-slide>Slide 2</swiper-slide>
+              <swiper-slide>Slide 3</swiper-slide>
+              <swiper-slide>Slide 4</swiper-slide>
+              <swiper-slide>Slide 5</swiper-slide>
+              <div class="swiper-pagination"
+                   slot="pagination"></div>
+              <div class="swiper-button-prev"
+                   slot="button-prev"></div>
+              <div class="swiper-button-next"
+                   slot="button-next"></div>
+            </swiper>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <!-- <el-pagination class="pagination"
+                   background
+                   @size-change="sizeChange"
+                   @current-change="currentChange"
+                   :current-page="pageChange.pageNum"
+                   :page-size="pageChange.pageSize"
+                   @next-click="nextClick"
+                   layout="total, prev, pager, next, jumper"
+                   :total="total">
+    </el-pagination> -->
+  </div>
+</template>
+<script>
+import tableMixin from '@/mixins/dealTable'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+export default {
+  mixins: [tableMixin],
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction'
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
+      },
+      total: 100,
+      pageChange: {
+        pageNum: 1,
+        pageSize: 10
+      }
+    }
+  },
+
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+    sizeChange (pageSize) {
+      this.pageChange.pageSize = pageSize
+      // 页数大小发生变化时，手动将当前页设置为1
+      this.pageChange.pageNum = 1
+    },
+    currentChange (pageNum) {
+      this.pageChange.pageNum = pageNum
+    },
+    nextClick (pageNum) {
+
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+@import "./index";
+</style>
